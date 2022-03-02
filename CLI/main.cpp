5be3787d13e96d8225d0ec3d72880ead36420c3b
FenrirBots <97492893+FenrirBots@users.noreply.github.com>
2022-03-02 08:13:42 +0000

    Redone CLI
    Redone all the logic as i clearly didnt know what i was doing last time.
    
    The updated version should now handle both positive and negative numbers.
    
    it should also handle items worth more than 1 unit in price.

--- CLI/main.cpp
+++ CLI/main.cpp
@@ -4,35 +4,61 @@
 // This is a very basic implamentation
 // this SHOULD handle the math properly
 
-float amount = 0.0f;
+int amount = 0;
 float price  = 0.0f;
 
 int main(int argc, char** argv) {
-    
-    std::cout << "Input amount: ";
 
-    while (std::cin >> amount) {
-        if (amount <= 0.0f) {
-            std::cout << std::endl << "Invalid amount.";
+    static bool  SaveTotal  = false;
+    static float SavedTotal = 0.0f;
+
+    while (true) {
+
+        float Amount     = 0.0f;
+        float Price      = 0.0f;
+        float Total      = 0.0f;
+
+        std::cout << "Input Amount: ";
+        std::cin >> Amount;
+        std::cout << "Input Price: ";
+        std::cin >> Price;
+
+        if (Amount == Price) {
+            Total = Price;
+        } else if (Amount < Price){
+            Total = Amount * Price;
+        } else {
+            Total = Amount / Price;
+        }
+
+        if (SavedTotal) {
+            std::cout << "SavedTotal: " << SavedTotal + Total << std::endl;
+        } else {
+            std::cout << "Total: " << Total << std::endl;
+        }
+
+        char c;
+        std::cout << "Save Total:  Y/N" << std::endl;
+        while (std::cin >> c) {
+            switch (c) {
+                case 'Y': case 'y':
+                    SavedTotal += Total;
+                    SaveTotal = true;
+                    break;
+
+                case 'N': case 'n':
+                    SavedTotal = 0;
+                    SaveTotal = false;
+                    break;
+
+                default:
+                    std::cout << "Save Total:  Y/N" << std::endl;
+                    continue;
+            }
+
             break;
         }
 
-        std::cout << std::endl << "Input price: ";
-        while (std::cin >> price) {
-            if (price <= 0.0f) {
-                std::cout << std::endl << "Invalid price.";
-                break;
-            }
-
-            if (price >= 10.0f) {
-                std::cout << std::endl << "WLS after selling: " << ((amount / 200) * price);
-                break;
-            } else {
-                std::cout << std::endl << "WLS after selling: " << (amount / price);
-                break;
-            }
-        }
-    }
-
-    return 0;
+        std::cout << std::endl;
+    }   
 }
